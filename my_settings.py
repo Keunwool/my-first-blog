79da7e7f44213f1ad65ea58f66d337bad603a290
comarts@kisvan.co.kr <comarts@kisvan.co.kr>
2021-01-13 11:10:45 +0900

    My Django Girls app, first commit

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+#my_settings.py
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'red',
+        'USER': 'admin',
+        'PASSWORD': 'qwe123!@#',
+        'HOST': '10.71.71.22',
+        'PORT': '3306',
+    }
+}
+SECRET_KEY = '#qh3fwne1btatgwun@7hx&(n+eh3@*)(^u)671z5x1^#=-t+$z'
